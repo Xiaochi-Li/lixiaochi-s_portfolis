d9a6ee4645596664259f73924689a81c7fc61638
Unknown <294188719@qq.com>
2017-06-26 06:37:31 +0800

    rescale images to square

--- js/js.js
+++ js/js.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
   widths = $('.works figure img');
 
-  for (var i=0;i<widths.length;i++){
-    var img = widths[i];
-    $(img).css("height",img.width);
-  }
+  
 });
